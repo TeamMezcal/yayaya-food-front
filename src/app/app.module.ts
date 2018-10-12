204d7a90ef1aeed623810b143e6341e22fd9e39a
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-10-12 13:05:20 +0200

    login html works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/misc/header/header.component';
 import { FooterComponent } from './components/misc/footer/footer.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/misc/register/register.component';
+import { LoginComponent } from './components/misc/login/login.component';
 
 @NgModule({
   declarations: [
