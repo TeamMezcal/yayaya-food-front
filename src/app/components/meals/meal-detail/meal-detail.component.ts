dfbd3e80e20d4cd6b6600d8fcdc0342e3c0e448c
Jose Bernat <jbf@iesat.com>
2018-10-13 16:49:50 +0200

    feat: meal-detail.component added

--- src/app/components/meals/meal-detail/meal-detail.component.ts
+++ src/app/components/meals/meal-detail/meal-detail.component.ts
@@ -1,15 +1,20 @@
+import { Meal } from './../../../shared/models/meal.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-meal-detail',
-  templateUrl: './meal-detail.component.html',
-  styleUrls: ['./meal-detail.component.css']
+  selector: 'app-meal-details',
+  templateUrl: './meal-details.component.html',
+  styleUrls: ['./meal-details.component.css']
 })
-export class MealDetailComponent implements OnInit {
-
-  constructor() { }
+export class MealDetailsComponent implements OnInit {
+  meal: Meal = new Meal();
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.data.subscribe(data => this.meal = data.meal);
   }
 
 }
